82d8c875ecd4e2c741b29832c3fa94b110303abe
unknown <kaxudi34801@gmail.com>
2023-04-15 23:18:09 +0500

    material icons add. material icon css add. material icon other components added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
+<template>
+  <v-card title="Convert text to speech" subtitle="Listen to those words!">
+  <v-card-actions>
+    <v-btn icon="mdi-play" variant="outlined"></v-btn>
+    <v-btn icon="mdi-pause" variant="outlined"></v-btn>
+    <v-btn icon="mdi-play-pause" variant="outlined"></v-btn>
+    <v-btn icon="mdi-stop" variant="outlined"></v-btn>
+  </v-card-actions>
+</v-card>  
+</template>
+
 <script setup>
+
 </script>
 
-<template>
-  
-</template>
 
 <style scoped>
 
--- src/main.js
+++ src/main.js
@@ -1,15 +1,27 @@
 import { createApp } from 'vue'
-import './style.css'
+import './assets/styles/main.css'
 import App from './App.vue'
 import { createPinia } from 'pinia';
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { aliases, fa } from 'vuetify/iconsets/fa'
+import { mdi } from 'vuetify/iconsets/mdi'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
 
 const vuetify = createVuetify({
-  components,
-  directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+            fa
+        }
+    },
+    components,
+    directives,
 })
 
 const app = createApp(App);
